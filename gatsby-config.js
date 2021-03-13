4c896da8d6838c30a6f1a936167e11b8a1a2c2be
danielraison@gmail.com <danielraison@gmal.com>
2021-03-13 11:12:03 +0100

    New Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,9 @@
 const path = require('path')
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -44,7 +48,7 @@ module.exports = {
         // The domain name of your Shopify shop. This is required.
         // Example: 'gatsby-source-shopify-test-shop' if your Shopify address is
         // 'gatsby-source-shopify-test-shop.myshopify.com'.
-        shopName: "graphql",
+        shopName: process.env.SHOP_NAME,
 
         // An API access token to your Shopify shop. This is required.
         // You can generate an access token in the "Manage private apps" section
@@ -52,7 +56,7 @@ module.exports = {
         // to select "Allow this app to access your storefront data using the
         // Storefront API".
         // See: https://help.shopify.com/api/custom-storefronts/storefront-api/getting-started#authentication
-        accessToken: "dd4d4dc146542ba7763305d71d1b3d38",
+        accessToken: process.env.SHOPIFY_ACCESS_TOKEN,
 
         // Set verbose to true to display a verbose output on `npm run develop`
         // or `npm run build`. This prints which nodes are being fetched and how
